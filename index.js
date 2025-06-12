695101f730f2ec0a985326457d2ab0ef2858685b
iamShimanto <shimantowebdeveloper@gmail.com>
2025-06-12 23:21:10 +0600

    style add

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoosh = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const Listing = require("./models/listing.js");
+const ejsMate = require("ejs-mate");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -11,6 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 main()
   .then(() => {
